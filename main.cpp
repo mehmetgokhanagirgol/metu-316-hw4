8a76e09f61c7178c7a710b2a7ecc106eefb6a4f8
mehmetgokhanagirgol <gokhanagirgol@gmail.com>
2022-06-15 19:56:47 +0300

    greedy algorithm

--- main.cpp
+++ main.cpp
@@ -1,47 +1,98 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 #include <algorithm>
 
 using namespace std;
 
+struct breakpointVec{
+    int x;
+    bool breakpoint;    
+};
+
 void takeInput(vector<int> &vec){
-    int n;
-    while(cin >> n){
-        vec.push_back(n);
+    int temp;
+    while(!cin.eof()){
+        cin >> temp;
+        vec.push_back(temp);
+    }
+    vec.push_back((int)vec.size());
+}
+
+void constructBreakpointVec(vector<int> vec, vector<breakpointVec> &breakpointVec){
+    for(unsigned long i = 0; i < vec.size(); ++i){
+        breakpointVec.push_back({vec[i], false});
     }
 }
 
+void changeBreakpointVec(vector<int> vec, vector<breakpointVec> &breakpointVec){
+    for(unsigned long i = 0; i < vec.size(); ++i){
+        breakpointVec[i].x = vec[i];
+    }
+} 
 
-bool dfs(vector<int> &vec, int limit, vector<int> targetVec, vector<pair<int,int>> &stack){
-    if(vec == targetVec){
+int checkBreakpoints(vector<breakpointVec> &vec, int breakpointCount){
+    breakpointCount = 0;
+    for(unsigned long i = 0; i < vec.size()-1; ++i){
+        if(abs(vec[i].x-vec[i+1].x) != 1){
+            vec[i].breakpoint = true;
+            breakpointCount++;
+        }
+        else{
+            vec[i].breakpoint = false;
+            breakpointCount--;
+        }
+    }
+    return breakpointCount;
+}
+
+
+bool dfs(vector<breakpointVec> &breakVec, vector<int> &vec, vector<int> target, int &count, vector<pair<int, int>> &result){
+    if(vec == target){
         return true;
     }
     else{
-        for(unsigned long i = 0; i < vec.size(); ++i){
-            for(unsigned long j = i+1; j < vec.size(); ++j){
-                reverse(vec.begin()+i, vec.begin()+j);
-                stack.push_back({i,j});
-                if(dfs(vec, limit, targetVec, stack)){
-                    return true;
+        for(int i = 0; i < (int)breakVec.size(); ++i){
+            if(breakVec[i].breakpoint == true){
+                for(int j = 0; j < (int)breakVec.size(); ++j){
+                    if(breakVec[j].x == i+1){
+                        for(;j < (int)breakVec.size();++j){
+                            if(breakVec[j].breakpoint == true)
+                                break;
+                        }
+                        reverse(vec.begin()+i+1, vec.begin()+j+1);
+                        changeBreakpointVec(vec, breakVec);
+                        checkBreakpoints(breakVec, count);
+                        count++;
+                        result.push_back({i+1,j});
+                        if(dfs(breakVec, vec, target, count,result)){
+                            return true;
+                        }
+                    }
                 }
-                stack.pop_back();
-                reverse(vec.begin()+i, vec.begin()+j);
             }
         }
         return false;
     }
 }
 
+
 int main(){
-    vector<int> vec;
-    vector<int> targetVec;
-    vector<pair<int,int>> stack;
+    vector<int> vec = {0};
+    vector<int> target;
+    vector<pair<int, int>> result;
+    vector<breakpointVec> breakVec;
+    int count = 0;
+    int breakpointCount = 0;
     takeInput(vec);
-    sort(vec.begin(), vec.end());
-    targetVec = vec;
-    for(int limit = 1; dfs(vec, limit, targetVec, stack) == false; ++limit);
-    for(unsigned long i = 0; i < stack.size(); ++i){
-        cout << stack[i].first << " " << stack[i].second << endl;
+    target = vec;
+    sort(target.begin(), target.end());
+    constructBreakpointVec(vec, breakVec);
+    breakpointCount = checkBreakpoints(breakVec, breakpointCount);
+    dfs(breakVec, vec, target, count, result);
+    cout <<  count << endl;
+    for(auto i = 0; i < result.size(); ++i){
+        cout << result[i].first << " " << result[i].second << endl;
     }
     return 0;
 }
